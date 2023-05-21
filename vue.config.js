459dae75565ba4bfc66eca51875b4fcbe92ff355
tripiesskepsis <134172263+tripiesskepsis@users.noreply.github.com>
2023-05-21 04:27:39 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,2 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports =  {publicPath: 'tripiesskepsis.github.io',};
+module.exports =  {publicPath: 'tripiesskepsis/tripiesskepsis.github.io',};
